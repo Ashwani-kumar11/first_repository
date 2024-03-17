937c2beb01c9534e5b14a3b49772e630c7ce8e98
ashwani-kumar11 <anshkumar6998@gmail.com>
2024-03-17 22:07:06 +0530

    first commit

---
+++ js_59calculator.js
@@ -0,0 +1,12 @@
+/*
+Create a faulty calculator
+1- takes two numbers as inputs;
+2- performs wrong operations
+   + ----> -
+   * ----> +
+   - ----> /
+   / ----> **(exponent)
+
+3- performs wrong operation only for the 10% of the times
+ */
+
